9b3c1555a4817317684cfc4771f4c97eeb16fe5b
Adam Tannir <atannir@vonaco>
2015-09-02 17:44:48 -0400

    Just getting started

---
+++ rest api/wordstore_rest.js
@@ -0,0 +1,13 @@
+// REST API to the "word store and count" service
+//
+//
+
+// Load HTTP module so we have something to talk to/with
+var http = require('http');
+
+http.createServer(function (req, res) {
+    console.log(req.method + ' request received');
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end(); //echo
+}).listen(8000); //so we don't step on existing open ports nor require root permissions
+
