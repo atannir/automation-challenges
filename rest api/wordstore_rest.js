d7795a44831a8ad005c4fabaa910a4a24ca4b800
Adam Tannir <atannir@vonaco>
2015-09-10 16:12:50 -0400

    Started use of sqlite3, code runs, no link between db and requests

--- rest api/wordstore_rest.js
+++ rest api/wordstore_rest.js
@@ -2,8 +2,24 @@
 //
 //
 
+var debuglevel = 0;
+
+
 // Load HTTP module so we have something to talk to/with
 var http = require('http');
+
+// Load in SQLite and set up db
+var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database("test.db");
+var db = new sqlite3.Database(":memory:"); //default mode of readwrite | create
+
+db.serialize(function(){
+    db.run("CREATE TABLE if not exists rest_words (word TEXT UNIQUE, count INTEGER)");
+    console.log("DB created");
+    
+});
+
+
 
 http.createServer(function (req, res) {
     console.log(req.method + ' request received');
