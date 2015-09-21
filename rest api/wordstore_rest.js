f9518b8465d156c02e6e1beedfc2103e8d3128fb
Adam Tannir <atannir@vonaco>
2015-09-21 11:51:45 -0400

    minor update, adding first db queries

--- rest api/wordstore_rest.js
+++ rest api/wordstore_rest.js
@@ -10,20 +10,31 @@ var http = require('http');
 
 // Load in SQLite and set up db
 var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database("test.db");
+// var db = new sqlite3.Database("test.db"); //later, if we want to persist
 var db = new sqlite3.Database(":memory:"); //default mode of readwrite | create
+// we could have gotten away with a simple hash but I wanted to try out sqlite
+
 
 db.serialize(function(){
-    db.run("CREATE TABLE if not exists rest_words (word TEXT UNIQUE, count INTEGER)");
+    db.run("CREATE TABLE if not exists words (word TEXT UNIQUE, count INTEGER DEFAULT 1)");
     console.log("DB created");
     
 });
+
+//removed separate queries for with/without counts to simplify code.
+var getWord = db.prepare('SELECT word, count from words where word = (?)');
+var insertWord = db.prepare('INSERT INTO words (word) VALUES (?)');
+//var incrementWordCount = db.prepare('');
+var getAllWords = db.prepare('SELECT word, count from words');
+
+
+var oneWordError = '{ "error": "PUT requests must be one word in length" }';
 
 
 
 http.createServer(function (req, res) {
     var postdata = '';
-    // needed to init with '' to not get undefined
+    // needed to init with '' to not get undefined later
     
     console.log(req.method + ' request received');
     res.writeHead(200, {'Content-type': 'text/plain'});
